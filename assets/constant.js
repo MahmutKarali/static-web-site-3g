a42371f4991ec18c4e127ae101b7af8665680f92
MahmutKarali <ceng.karali@gmail.com>
2023-12-20 18:01:54 +0300

    'push'

---
+++ assets/constant.js
@@ -0,0 +1,68 @@
+var address = "Mevlana Mahallesi Yonca Sok. No:2/4 Ataşehir İstanbul"
+var language = {
+	"en": {
+		"short_language": "EN",
+		"header_shop": "SHOP",
+		"contact": "Contact",
+		"top_header_about": "About",
+		"top_header_call_phone": "Call: +90 212 801 75 75",
+
+		//header
+		"header_homepage": "HOME",
+		"header_about": "ABOUT",
+		"header_services": "SERVICES",
+		"header_portfolio": "PORTFOLIO",
+		"header_contact": "CONTACT",
+
+		//contact
+		"contact_header": "Contact",
+		"contact_info": "Contact Info",
+		"contact_info_message": "You can contact us via our full address, office phone and mail.",
+		"contact_central_office": "Central Office",
+		"contact_have_question": "Have a Question?",
+		"contact_have_question_detail": "To contact our relevant teams, fill in the information below and send it to us.",
+		"contact_form_name": "Name *",
+		"contact_form_email": "E-mail *",
+		"contact_form_phone": "Phone *",
+		"contact_form_subject": "Subject *",
+		"contact_form_messsage": "Message *",
+		"contact_form_send": "Send",
+
+		//about
+		"about_our_vision": "Our Vision",
+		"about_our_mission": "Our Mission",
+	},
+	"tr": {
+		"short_language": "TR",
+		"header_shop": "Alisveris",
+		"contact": "Iletisim",
+		"top_header_contact": "Iletisim",
+		"top_header_about": "Hakkimizda",
+		"top_header_call_phone": "Ara: +90 212 801 75 75",
+
+		//header
+		"header_homepage": "ANA SAYFA",
+		"header_about": "HAKKIMIZDA",
+		"header_services": "HİZMETLERİMİZ",
+		"header_portfolio": "PORTFOLYO",
+		"header_contact": "İLETİŞİM",
+
+		//contact
+		"contact_header": "İletişim",
+		"contact_info": "İletişim Bilgileri",
+		"contact_info_message": "Açık adresimiz, ofis telefonumuz ve mail aracılığı ile bizimle iletişime geçebilirsiniz.",
+		"contact_central_office": "Merkez Ofis",
+		"contact_have_question": "Sorunuz Mu Var?",
+		"contact_have_question_detail": "İlgili ekiplerimize iletişime geçmek için aşağıdaki bilgileri doldurup bize gönderin.",
+		"contact_form_name": "İsim *",
+		"contact_form_email": "E-posta *",
+		"contact_form_phone": "Telefon *",
+		"contact_form_subject": "Konu *",
+		"contact_form_messsage": "Mesaj *",
+		"contact_form_send": "Gönder",
+
+		//about
+		"about_our_vision": "Vizyonumuz",
+		"about_our_mission": "Misyonumuz",
+	},
+};
